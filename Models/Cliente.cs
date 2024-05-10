7b4ca097cb1fd5909846ef9da1cecc234035a1a8
Matheus <limarios18@outlook.com>
2024-05-10 18:00:14 -0300

    criação da classe model de Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿namespace APITechCoffee.Model
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome_fantasia { get; set; }
+        public string razao_social { get; set; }
+        public string cpf { get; set; }
+        public string cnpj { get; set;  }
+        public string inscricao_estadual { get; set; }
+        public string inscricao_municial { get; set; }
+        public bool iss_retido { get; set; }
+        public string telefone { get; set; }
+        public string celular { get; set; }
+        public string email { get; set; }
+        public DateTime data_cadastro { get; set; }
+        public string endereco { get; set; }
+        public string estado { get; set; }
+        public string bairro { get; set; }
+        public string cidade { get; set; }
+        public string pais { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }
+        public string ramo_atuacao { get; set; }
+
+
+
+    }
+}
